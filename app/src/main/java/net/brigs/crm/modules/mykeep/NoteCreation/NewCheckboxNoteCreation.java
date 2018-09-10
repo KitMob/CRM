45778c670eeebdba0b58f4d2cced3f425f178899
denis <deoniisii@gmail.com>
2018-09-11 02:31:05 +0300

    add checkbox_add icons

---
+++ app/src/main/java/net/brigs/crm/modules/mykeep/NoteCreation/NewCheckboxNoteCreation.java
@@ -0,0 +1,15 @@
+package net.brigs.crm.modules.mykeep.NoteCreation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.brigs.crm.R;
+
+public class NewCheckboxNoteCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_checkbox_note_creation);
+    }
+}
