ba2ba03d5948146fc2d4f277ad592af1243ea461
denis <deoniisii@gmail.com>
2018-09-13 14:04:24 +0300

    I made a draft copy for creation of the list

--- app/src/main/java/net/brigs/crm/modules/mykeep/NoteCreation/NewCheckboxNoteCreation.java
+++ app/src/main/java/net/brigs/crm/modules/mykeep/NoteCreation/NewCheckboxNoteCreation.java
@@ -1,22 +1,20 @@
 package net.brigs.crm.modules.mykeep.NoteCreation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 import net.brigs.crm.R;
 
 public class NewCheckboxNoteCreation extends AppCompatActivity implements OnClickListener {
     private EditText editText, textCheckBox;
-    private TextView editTexttextAddCheckBox;
+    private TextView editTexttextAddCheckBox, textViewAdCheckBox;
     private CheckBox checkBox;
     private ImageView imageViewAddCheckBox, imageViewDelCheckBox;
 
@@ -33,14 +31,16 @@ public class NewCheckboxNoteCreation extends AppCompatActivity implements OnClic
         editText = findViewById(R.id.new_checkbox_note_creation_title_edit_text);
 
         checkBox = findViewById(R.id.new_checkbox_note_creation_check_box);
-        textCheckBox= findViewById(R.id.new_checkbox_note_creation_text_check_box);
+        textCheckBox = findViewById(R.id.new_checkbox_note_creation_text_check_box);
         imageViewDelCheckBox = findViewById(R.id.image_button_dell_check_box);
 
         imageViewAddCheckBox = findViewById(R.id.imageView_add_check_box);
+        textViewAdCheckBox = findViewById(R.id.text_view__add_check_box);
         editTexttextAddCheckBox = findViewById(R.id.text_view__add_check_box);
 
 
         imageViewAddCheckBox.setOnClickListener(this);
+        textViewAdCheckBox.setOnClickListener(this);
 //TODO
 
     }
@@ -49,26 +49,34 @@ public class NewCheckboxNoteCreation extends AppCompatActivity implements OnClic
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.imageView_add_check_box:
-                CheckBox newCheckBox = new CheckBox(this);
+                addChecboxPoint();
 
-                TextView newTextCheckBox = new TextView(this);
-                newTextCheckBox.setBackground(textCheckBox.getBackground());
+                break;
 
-
-                ImageView newImageViewDelCheckBox = new ImageView(this);
-                newImageViewDelCheckBox.setImageResource(R.drawable.checkbox_dell);
-                newImageViewDelCheckBox.setClickable(true);
-
-                int count = 0;
-//                newCheckboxNoteCreationTableLayout.removeView(imageViewAddCheckBox);
-//                newCheckboxNoteCreationTableLayout.removeView(editTexttextAddCheckBox);
-                newCheckboxNoteCreationTableLayout.addView(newCheckBox);
-                newCheckboxNoteCreationTableLayout.addView(newTextCheckBox);
-                newCheckboxNoteCreationTableLayout.addView(newImageViewDelCheckBox);
-
+            case R.id.text_view__add_check_box:
+                addChecboxPoint();
                 break;
 
         }
 
+    }
+
+    private void addChecboxPoint() {
+        CheckBox newCheckBox = new CheckBox(this);
+
+        TextView newTextCheckBox = new TextView(this);
+        newTextCheckBox.setBackground(textCheckBox.getBackground());
+
+
+        ImageView newImageViewDelCheckBox = new ImageView(this);
+        newImageViewDelCheckBox.setImageResource(R.drawable.checkbox_dell);
+        newImageViewDelCheckBox.setClickable(true);
+
+        int count = 0;
+//                newCheckboxNoteCreationTableLayout.removeView(imageViewAddCheckBox);
+//                newCheckboxNoteCreationTableLayout.removeView(editTexttextAddCheckBox);
+        newCheckboxNoteCreationTableLayout.addView(newCheckBox);
+        newCheckboxNoteCreationTableLayout.addView(newTextCheckBox);
+        newCheckboxNoteCreationTableLayout.addView(newImageViewDelCheckBox);
     }
 }
