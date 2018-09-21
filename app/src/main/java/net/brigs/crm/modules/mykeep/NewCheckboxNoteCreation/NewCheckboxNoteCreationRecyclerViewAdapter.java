905826061bed4b2261d7dfb457dbdbe5c902d008
denis <deoniisii@gmail.com>
2018-09-21 14:07:46 +0300

    refactoring

---
+++ app/src/main/java/net/brigs/crm/modules/mykeep/NewCheckboxNoteCreation/NewCheckboxNoteCreationRecyclerViewAdapter.java
@@ -0,0 +1,125 @@
+package net.brigs.crm.modules.mykeep.NewCheckboxNoteCreation;
+
+import android.annotation.SuppressLint;
+import android.graphics.Paint;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import net.brigs.crm.R;
+
+import java.util.List;
+
+public class NewCheckboxNoteCreationRecyclerViewAdapter extends RecyclerView.Adapter<NewCheckboxNoteCreationRecyclerViewAdapter.NewCheckboxNoteCreationViewHolders> {
+
+    private List<NewCheckboxNoteCreationObjects> checkboxNoteList;
+    private ViewGroup _parent;
+    public static OnItemClickListener listener;
+
+    public NewCheckboxNoteCreationRecyclerViewAdapter(List<NewCheckboxNoteCreationObjects> itemList) {
+
+        checkboxNoteList = itemList;
+    }
+
+    @Override
+    public NewCheckboxNoteCreationViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        @SuppressLint("InflateParams")
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_checkbox_note_creation_list, null);
+
+        _parent = parent;
+        return new NewCheckboxNoteCreationViewHolders(layoutView);
+    }
+
+    @Override
+    public void onBindViewHolder(NewCheckboxNoteCreationViewHolders holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if (checkboxNoteList != null)
+            return checkboxNoteList.size();
+        return 0;
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+
+    public void addItem(int position, NewCheckboxNoteCreationObjects newCheckboxNoteCreationObjects) {
+        this.checkboxNoteList.add(position, newCheckboxNoteCreationObjects);
+    }
+
+    public void deleteItem(int position) {
+        this.checkboxNoteList.remove(position);
+    }
+
+
+    public class NewCheckboxNoteCreationViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private final RecyclerView recyclerViewNewCheckboxCoteCreationList;
+        private CheckBox checkBox;
+        private EditText text;
+        private ImageView imageButtonDell;
+
+        public NewCheckboxNoteCreationViewHolders(View itemView) {
+            super(itemView);
+
+            recyclerViewNewCheckboxCoteCreationList = itemView.findViewById(R.id.new_checkbox_note_creation_recycler_view);
+            checkBox = itemView.findViewById(R.id.new_checkbox_note_creation_check_box);
+            checkBox.setId(getLayoutPosition());
+            text = itemView.findViewById(R.id.new_checkbox_note_creation_text_check_box);
+            imageButtonDell = itemView.findViewById(R.id.image_button_dell_check_box);
+            imageButtonDell.setOnClickListener(this);
+            checkBox.setOnClickListener(this);
+
+            itemView.setOnClickListener(this);
+        }
+
+
+        @SuppressLint("ResourceAsColor")
+        @Override
+        public void onClick(View view) {
+            //TODO dell
+
+            Toast.makeText(view.getContext(), "Clicked Position  = " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+
+            if (checkBox.isChecked() == true) {
+
+                text.setPaintFlags(text.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            } else if (checkBox.isChecked() == false) {
+                //TODO color
+                Log.d("MyLog", " in");
+                text.setPaintFlags(0);
+            }
+
+            if (view.equals(imageButtonDell)) {
+                DeleteItem(getAdapterPosition());
+            }
+        }
+
+
+    }
+
+    private void DeleteItem(int position) {
+       deleteItem(position);
+       notifyItemRemoved(position);
+    }
+
+}
+
+
