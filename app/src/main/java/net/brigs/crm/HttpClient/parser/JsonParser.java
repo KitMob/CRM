202c778fb84a07125135e53a7db59fc49f0b3279
denis <deoniisii@gmail.com>
2018-11-01 14:22:21 +0200

    crate JsonParser

--- app/src/main/java/net/brigs/crm/HttpClient/parser/JsonParser.java
+++ app/src/main/java/net/brigs/crm/HttpClient/parser/JsonParser.java
@@ -1,17 +1,28 @@
 package net.brigs.crm.HttpClient.parser;
 
 import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class JsonParser {
 
     private boolean success;
     private String user;
 
-    public User getUser(String response) throws JSONException {
-        JSONObject userJson = new JSONObject(response);
-        success = userJson.getBoolean("success");
-        user  = String.valueOf(userJson.names());
+    public User getUser(String response) throws JSONException{
+        JSONParser parser = new JSONParser();
+        JSONObject in = null;
+        try {
+            in = (JSONObject) parser.parse(response);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        success = (boolean) in.get("success");
+
+        user = in.get("user").toString();
         return new User(success, user);
     }
 }
