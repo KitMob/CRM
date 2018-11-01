3f8be2f17c1077ccc17da7375f85ef5196043028
denis <deoniisii@gmail.com>
2018-11-01 18:24:18 +0200

    Made anticipation

--- app/src/main/java/net/brigs/crm/HttpClient/ShowLog.java
+++ app/src/main/java/net/brigs/crm/HttpClient/ShowLog.java
@@ -1,18 +1,7 @@
 package net.brigs.crm.HttpClient;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -27,11 +16,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-
-public class ShowLog extends AppCompatActivity implements Runnable {
+public class ShowLog extends AppCompatActivity {
 
     public static final String APPLICATION_LOG_NAME = "application_log.txt";
     private final String LOG_TAG = "myLogs";
@@ -53,70 +39,25 @@ public class ShowLog extends AppCompatActivity implements Runnable {
 
         super.onCreate(savedInstanceState);
 
-        if (!checkPermission()) {
-            openActivity();
-        } else {
-            if (checkPermission()) {
-                requestPermissionAndContinue();
-            } else {
-                openActivity();
-            }
+        setContentView(R.layout.activity_show_log);
+
+        textViewLog = findViewById(R.id.log_TextView);
+        Intent intent = getIntent();
+
+        String file= intent.getStringExtra("File");
+        if (file != null) {
+            aut = readFile(file);
+            textViewLog.append(aut);
         }
 
-
-        aut = " ";
 
 
     }
 
-
-    private Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            textViewLog = findViewById(R.id.log_TextView);
-            textViewLog.append(logFaille);
-        }
-    };
-
-    @Override
-    public void run() {
-        this.pathname = writeFileSD(APPLICATION_LOG_NAME).getAbsolutePath() + APPLICATION_LOG_NAME;
-
-        Log.d(LOG_TAG, "Start trend");
-        client = new Client();
-        uri = "https://brigs.top/login";
-        email = "android@mail.com";
-        password = "1234";
-
-        aut += "\n post to  uri: " + uri +
-                "\n email: " + email +
-                "\n password: " + password + "\n";
+    //TODO допелить
 
 
-        writeLog(pathname);
-
-
-        handler.sendEmptyMessage(0);
-
-    }
-
-    private void writeLog(String pathname) {
-        String rd = null;
-        try {
-            rd = client.setPost(uri, email, password).toString();
-            String answer = rd;
-                aut += "\n answer" + answer; //TODO
-            logFaille = readFile(appendLog.appendLog(aut, pathname));
-        } catch (IOException e) {
-            String messageError = new GetMessageError().getMessageError(e);
-            aut += messageError;
-            Log.e(LOG_TAG, "appendLog " + messageError);
-            e.printStackTrace();
-
-
-        }
-    }
-
-    private String readFile(File file) {
+    private String readFile(String file) {
         Log.d(LOG_TAG, "readFile ");
 
 
@@ -140,116 +81,6 @@ public class ShowLog extends AppCompatActivity implements Runnable {
         Log.d(LOG_TAG, "readFile text: \n" + text);
 
         return String.valueOf(text);
-    }
-
-
-    private File writeFileSD(String applicationLogName) {
-        File sdPath = null;
-        Log.d(LOG_TAG, "writeFileSD ");
-
-        // проверяем доступность SD
-        if (!Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED)) {
-            sdPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-
-            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
-        } else {
-            // получаем путь к SD
-            sdPath = Environment.getExternalStorageDirectory();
-            // добавляем свой каталог к пути
-            sdPath = new File(sdPath.getAbsolutePath() + "/." + getPackageName() + "/logs/");
-            // создаем каталог
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                Log.d(LOG_TAG, "writeFileSD  PERMISSION_GRANTED");
-                if (!sdPath.exists()) {
-                    sdPath.mkdirs();
-                }
-                File sdFile = new File(sdPath, applicationLogName);
-                try {
-                    sdFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return sdPath;
-        }
-
-        return sdPath;
-    }
-
-
-    private static final int PERMISSION_REQUEST_CODE = 200;
-
-    private boolean checkPermission() {
-
-        return ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermissionAndContinue() {
-        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, WRITE_EXTERNAL_STORAGE)
-                    && ActivityCompat.shouldShowRequestPermissionRationale(this, READ_EXTERNAL_STORAGE)) {
-                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-                alertBuilder.setCancelable(true);
-                alertBuilder.setTitle(getString(R.string.permission_necessary));
-                alertBuilder.setMessage(R.string.storage_permission_is_encessary_to_wrote_event);
-                alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions(ShowLog.this, new String[]{WRITE_EXTERNAL_STORAGE
-                                , READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-                    }
-                });
-                AlertDialog alert = alertBuilder.create();
-                alert.show();
-                Log.e("", "permission denied, show dialog");
-            } else
-                ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE,
-                        READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-        } else {
-            openActivity();
-        }
-    }
-
-    //
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (permissions.length > 0 && grantResults.length > 0) {
-
-                boolean flag = true;
-                for (int i = 0; i < grantResults.length; i++) {
-                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                        flag = false;
-                    }
-                }
-                if (flag) {
-                    openActivity();
-                } else {
-                    finish();
-                }
-
-            } else {
-                finish();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    private void openActivity() {
-        //add your further process after giving permission or to download images from remote server.
-        setContentView(R.layout.activity_show_log);
-
-        Thread thread = new Thread(this);
-        thread.start();
-
-
     }
 
 
